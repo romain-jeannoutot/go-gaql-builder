891a918e376576f248356c658ffab4ad7b018db7
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2021-04-14 23:21:25 +0200

    fix(where): join with and instead of comma

--- where.go
+++ where.go
@@ -9,7 +9,7 @@ type where []string
 
 func (w where) String() string {
 	if len(w) > 0 {
-		return fmt.Sprintf("WHERE %s", strings.Join(w, ", "))
+		return fmt.Sprintf("WHERE %s", strings.Join(w, " AND "))
 	}
 
 	return ""
